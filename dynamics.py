7928d01abf2cd0367a43bc80fd9f20534e889f66
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2023-05-21 11:58:38 +0100

    UPD: add all main equations

--- dynamics.py
+++ dynamics.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import numpy as np
 
+
 class STI:
-    def __init__(self):
-        self.population = None # read from the file
-        # add respective columns
-        self.initial_infected = 10_000  # randomly assigned
+    def __init__(self, base_data):
+        self.population = base_data  # read from the file
 
         self.reciprocal_phi = 14  # days
+        self.phi = 1 / self.reciprocal_phi
+
         self.nu = 62.5  # percent
+
         self.reciprocal_gamma_a = 300  # days
-        self.reciprocal_gamma_s = 35  #days
+        self.gamma_a = 1 / self.reciprocal_gamma_a
+
+        self.reciprocal_gamma_s = 35  # days
+        self.gamma_s = 1 / self.reciprocal_gamma_s
+
         self.reciprocal_xi = 90  # days
+        self.xi = 1 / self.reciprocal_xi
+
         self.sigma = 3  # power law exponent
         self.e_g = 0.7  # age group mixing
         self.e_h = 0.3  # sex group mixing
@@ -24,42 +32,91 @@ class STI:
 
         self.alpha = 0.5  # fixme find the value
 
-        self.C = None # FIXME
+        self.C = None  # FIXME
         self.d = {'age': [16, 17, 18, 19,
-                     20, 21, 22, 23, 24,
-                     25, 26, 27, 28, 29,
-                     30, 31, 32, 33, 34,
-                     35, 36, 37, 38, 39,
-                     40, 41, 42, 43, 44,
-                     45, 46, 47, 48, 49,
-                     50, 51, 52, 53, 54,
-                     55, 56, 57, 58, 59,
-                     60, 61, 62, 63, 64,
-                     65, 66, 67, 68, 69,
-                     70, 71, 72, 73, 74,
-                     75, 76, 77, 78, 79,
-                     80, 81, 82, 83, 84,
-                     85, 86, 87, 88, 89,
-                     90, 91, 92, 93, 94,
-                     95, 96, 97, 98],
-             'spa_rate': [0.85, 0.85, 0.85, 0.85,
-                          1.2, 1.2, 1.2, 1.2, 1.2,
-                          0.61, 0.61, 0.61, 0.61, 0.61,
-                          0.33, 0.33, 0.33, 0.33, 0.33,
-                          0.25, 0.25, 0.25, 0.25, 0.25,
-                          0.19, 0.19, 0.19, 0.19, 0.19,
-                          0.14, 0.14, 0.14, 0.14, 0.14,
-                          0.095, 0.095, 0.095, 0.095, 0.095,
-                          0.065, 0.065, 0.065, 0.065, 0.065,
-                          0.045, 0.045, 0.045, 0.045, 0.045,
-                          0.033, 0.033, 0.033, 0.033, 0.033,
-                          0.025, 0.025, 0.025, 0.025, 0.025]}
+                          20, 21, 22, 23, 24,
+                          25, 26, 27, 28, 29,
+                          30, 31, 32, 33, 34,
+                          35, 36, 37, 38, 39,
+                          40, 41, 42, 43, 44,
+                          45, 46, 47, 48, 49,
+                          50, 51, 52, 53, 54,
+                          55, 56, 57, 58, 59,
+                          60, 61, 62, 63, 64,
+                          65, 66, 67, 68, 69,
+                          70, 71, 72, 73, 74,
+                          75, 76, 77, 78, 79,
+                          80, 81, 82, 83, 84,
+                          85, 86, 87, 88, 89,
+                          90, 91, 92, 93, 94,
+                          95, 96, 97, 98],
+                  'spa_rate': [0.85, 0.85, 0.85, 0.85,
+                               1.2, 1.2, 1.2, 1.2, 1.2,
+                               0.61, 0.61, 0.61, 0.61, 0.61,
+                               0.33, 0.33, 0.33, 0.33, 0.33,
+                               0.25, 0.25, 0.25, 0.25, 0.25,
+                               0.19, 0.19, 0.19, 0.19, 0.19,
+                               0.14, 0.14, 0.14, 0.14, 0.14,
+                               0.095, 0.095, 0.095, 0.095, 0.095,
+                               0.065, 0.065, 0.065, 0.065, 0.065,
+                               0.045, 0.045, 0.045, 0.045, 0.045,
+                               0.033, 0.033, 0.033, 0.033, 0.033,
+                               0.025, 0.025, 0.025, 0.025, 0.025]}
         self.l = pd.DataFrame(data=self.d).set_index('age').fillna(0)
-        self.N = None # total people per age group
+        self.N = self.population.copy()
+
+        self.population['S'] = self.population['count'] - 100
+
+        self.population['E'] = 0
+
+        self.population['Ia'] = 50
+        self.population['Is'] = 50
+
+        self.population['W'] = 0
+        self.population['R'] = 0
+        self.population['E2'] = 0
+        self.population['Ia2'] = 0
+        self.population['Is2'] = 0
+        self.population['W2'] = 0
+
+        _n = {'age': [16, 17, 18, 19,
+                      20, 21, 22, 23, 24,
+                      25, 26, 27, 28, 29,
+                      30, 31, 32, 33, 34,
+                      35, 36, 37, 38, 39,
+                      40, 41, 42, 43, 44,
+                      45, 46, 47, 48, 49,
+                      50, 51, 52, 53, 54,
+                      55, 56, 57, 58, 59,
+                      60, 61, 62, 63, 64,
+                      65, 66, 67, 68, 69,
+                      70, 71, 72, 73, 74,
+                      75, 76, 77, 78, 79,
+                      80, 81, 82, 83, 84,
+                      85, 86, 87, 88, 89,
+                      90, 91, 92, 93, 94,
+                      95, 96, 97, 98],
+              'coital_frequency': [3.9, 3.9, 3.9, 3.9,
+                                   3.5, 3.5, 3.5, 3.5, 3.5,
+                                   3.2, 3.2, 3.2, 3.2, 3.2,
+                                   2.9, 2.9, 2.9, 2.9, 2.9,
+                                   2.5, 2.5, 2.5, 2.5, 2.5,
+                                   2.2, 2.2, 2.2, 2.2, 2.2,
+                                   1.9, 1.9, 1.9, 1.9, 1.9,
+                                   1.5, 1.5, 1.5, 1.5, 1.5,
+                                   1.2, 1.2, 1.2, 1.2, 1.2,
+                                   0.87, 0.87, 0.87, 0.87, 0.87,
+                                   0.53, 0.53, 0.53, 0.53, 0.53,
+                                   0.2, 0.2, 0.2, 0.2, 0.2]}
+        self.n = pd.DataFrame(data=_n).set_index('age').fillna(0)
 
     # TODO replace with classes?
     def delta_S(self, age: int, sexual_risk: int):
-        return -self.get_lambda(age, sexual_risk) * self.get_S(age, sexual_risk) + (1/self.reciprocal_gamma_a) * (1 - self.h_a) * self.get_Ia(age, sexual_risk) + (1/self.reciprocal_gamma_s) * (1 - self.h_s) * self.get_Is(age, sexual_risk)
+        return -self.get_lambda(age, sexual_risk) * self.get_S(age,
+                                                               sexual_risk) + self.gamma_a * (
+                1 - self.h_a) * self.get_Ia(age,
+                                            sexual_risk) + self.gamma_s * (
+                1 - self.h_s) * self.get_Is(age, sexual_risk)
 
     def get_S(self, age: int, sexual_risk: int) -> float:
         pass
@@ -68,7 +125,9 @@ class STI:
         pass
 
     def delta_E(self, age: int, sexual_risk: int) -> float:
-        return self.get_lambda(age, sexual_risk) * self.get_S(age, sexual_risk) - (1 / self.reciprocal_phi) * self.get_E(age, sexual_risk)
+        return self.get_lambda(age, sexual_risk) * self.get_S(age,
+                                                              sexual_risk) - self.phi * self.get_E(
+            age, sexual_risk)
         # TODO check for mu with no indices
 
     def get_E(self, age: int, sexual_risk: int) -> float:
@@ -78,7 +137,9 @@ class STI:
         pass
 
     def delta_Ia(self, age: int, sexual_risk: int):
-        return self.nu * (1 / self.reciprocal_phi) * self.get_E(age, sexual_risk) - self.get_Ia(age, sexual_risk)/self.reciprocal_gamma_s
+        return self.nu * self.phi * self.get_E(age,
+                                               sexual_risk) - self.gamma_s * self.get_Ia(
+            age, sexual_risk)
 
     def get_Ia(self, age: int, sexual_risk: int) -> float:
         pass
@@ -87,7 +148,9 @@ class STI:
         pass
 
     def delta_Is(self, age: int, sexual_risk: int):
-        return (1 - self.nu) * (1/self.reciprocal_phi) * self.get_E(age, sexual_risk) - self.get_Is(age, sexual_risk) / self.reciprocal_gamma_s
+        return (1 - self.nu) * self.phi * self.get_E(age,
+                                                     sexual_risk) - self.gamma_s * self.get_Is(
+            age, sexual_risk)
 
     def get_Is(self, age: int, sexual_risk: int) -> float:
         pass
@@ -96,7 +159,9 @@ class STI:
         pass
 
     def delta_W(self, age: int, sexual_risk: int) -> float:
-        return  (1/self.reciprocal_gamma_a) * self.h_a * self.get_Ia(age, sexual_risk) + (1 / self.reciprocal_gamma_s) * self.h_s * self.get_Is(age, sexual_risk) - (1 / self.reciprocal_xi) * self.get_W(age, sexual_risk)
+        return self.gamma_a * self.h_a * self.get_Ia(age,
+                                                     sexual_risk) + self.gamma_s * self.h_s * self.get_Is(
+            age, sexual_risk) - self.xi * self.get_W(age, sexual_risk)
 
     def get_W(self, age: int, sexual_risk: int) -> float:
         pass
@@ -105,7 +170,13 @@ class STI:
         pass
 
     def delta_R(self, age: int, sexual_risk: int) -> float:
-        return (1 / self.reciprocal_xi) * (self.get_W(age, sexual_risk) + self.get_W2(age, sexual_risk)) - ((1 - self.alpha) * self.get_lambda(age, sexual_risk)) * self.get_R(age, sexual_risk) + (1 / self.reciprocal_gamma_a) * (1 - self.h_a) * self.get_Ia2(age, sexual_risk) + (1 / self.reciprocal_gamma_s) * (1 - self.h_s) * self.get_Is2(age, sexual_risk)
+        return self.xi * (self.get_W(age, sexual_risk) + self.get_W2(age,
+                                                                     sexual_risk)) - (
+                (1 - self.alpha) * self.get_lambda(age,
+                                                   sexual_risk)) * self.get_R(
+            age, sexual_risk) + self.gamma_a * (1 - self.h_a) * self.get_Ia2(
+            age, sexual_risk) + self.gamma_s * (1 - self.h_s) * self.get_Is2(
+            age, sexual_risk)
 
     def get_R(self, age: int, sexual_risk: int) -> float:
         pass
@@ -114,7 +185,10 @@ class STI:
         pass
 
     def delta_E2(self, age: int, sexual_risk: int) -> float:
-        return (1 - self.alpha) * self.get_lambda(age, sexual_risk) * self.get_R(age, sexual_risk) - (1 / self.reciprocal_phi) * self.get_E2(age, sexual_risk)
+        return (1 - self.alpha) * self.get_lambda(age,
+                                                  sexual_risk) * self.get_R(age,
+                                                                            sexual_risk) - self.phi * self.get_E2(
+            age, sexual_risk)
 
     def get_E2(self, age: int, sexual_risk: int) -> float:
         pass
@@ -123,7 +197,9 @@ class STI:
         pass
 
     def delta_Ia2(self, age: int, sexual_risk: int) -> float:
-        return self.nu * (1/self.reciprocal_phi) * self.get_E2(age, sexual_risk) - (1 / self.reciprocal_gamma_a) * self.get_Ia2(age, sexual_risk)
+        return self.nu * self.phi * self.get_E2(age,
+                                                sexual_risk) - self.gamma_a * self.get_Ia2(
+            age, sexual_risk)
 
     def get_Ia2(self, age: int, sexual_risk: int) -> float:
         pass
@@ -132,7 +208,9 @@ class STI:
         pass
 
     def delta_Is2(self, age: int, sexual_risk: int) -> float:
-        return (1 - self.nu) * (1 / self.reciprocal_phi) * self.get_E2(age, sexual_risk) - (1 / self.reciprocal_gamma_s) * self.get_Is2(age, sexual_risk)
+        return (1 - self.nu) * self.phi * self.get_E2(age,
+                                                      sexual_risk) - self.gamma_s * self.get_Is2(
+            age, sexual_risk)
 
     def get_Is2(self, age: int, sexual_risk: int) -> float:
         pass
@@ -141,7 +219,9 @@ class STI:
         pass
 
     def delta_W2(self, age: int, sexual_risk: int) -> float:
-        return (1 / self.reciprocal_gamma_a) * self.h_a * self.get_Ia2(age, sexual_risk) + (1 / self.reciprocal_gamma_s) * self.h_s * self.get_Is2(age, sexual_risk) - (1 / self.reciprocal_xi) * self.get_W2(age, sexual_risk)
+        return self.gamma_a * self.h_a * self.get_Ia2(age,
+                                                      sexual_risk) + self.gamma_s * self.h_s * self.get_Is2(
+            age, sexual_risk) - self.xi * self.get_W2(age, sexual_risk)
 
     def get_W2(self, age: int, sexual_risk: int) -> float:
         pass
@@ -149,21 +229,47 @@ class STI:
     def update_W2(self, age: int, sexual_risk: int) -> None:
         pass
 
-    def get_l(self, age:int) -> float:
+    def get_l(self, age: int) -> float:
         return self.l.loc[age, 'spa_rate']
 
     def get_rho(self, age: int, sexual_risk: int) -> float:
         return self.C * self.get_l(age) * np.power(sexual_risk, self.sigma)
 
-    def get_G(self, x: int, age: int) -> float:
-        pass
+    def get_G(self, x: int, j: int) -> float:
+        k_ = 1  # todo only one sex risk group atm
+        t = self.get_rho(j, k_) * self.get_N(j, k_)
+        t1 = np.sum(
+            [self.get_rho(j, k_) * self.get_N(j, k_) for j in self.d['age']])
+        return self.e_g * np.identity(len(self.d['age']))[
+            self.d['age'].index(x), self.d['age'].index(j)] + (
+                    1 - self.e_g) * t / t1
 
-    def get_N(self, age:int) -> float:
-        if self.N is None:
-            self.N = self.population.groupby('age').count().to_frame()
-        return self.N.loc[age, :].values[0]  # fixme not correct?
+    def get_H(self, y: int, k: int) -> float:
+        return 1
 
-    def get_lambda(self,  age: int, sexual_risk: int) -> float:
-        pass
+    def get_N(self, age: int, sexual_risk: int) -> float:
+        return self.N.loc[age, :].values[0]
 
+    def get_lambda(self, age: int, sexual_risk: int) -> float:
+        # todo check s7 formula, there is some odd division
+        t = [self.get_G(age, j) * ((self.get_rho(j, 1) * self.get_qia(age,
+                                                                      sexual_risk) * (
+                                                self.get_Ia(j,
+                                                            1) + self.get_Ia2(j,
+                                                                              1)) + self.get_rho(
+            j, 1) * self.get_qis(age, sexual_risk) * (self.get_Is(j,
+                                                                  1) + self.get_Is2(
+            j, 1))) / (self.get_rho(j, 1) * self.get_N(j, 1))) for j in
+             self.d['age']]
+        return self.get_rho(age, sexual_risk) * np.sum(t)
 
+    def get_n(self, age: int, sexual_risk: int):
+        return self.n.loc[age]
+
+    def get_qia(self, age: int, sexual_risk: int):
+        return 1 - np.power(1 - self.p,
+                            self.m_ia * self.get_n(age, sexual_risk) * self.tau)
+
+    def get_qis(self, age: int, sexual_risk: int):
+        return 1 - np.power(1 - self.p,
+                            self.m_is * self.get_n(age, sexual_risk) * self.tau)
